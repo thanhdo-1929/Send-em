7370c884c0c97e5602fc06a281e72b7b138a429d
Thanhdo <dotuanthanhh@gmail.com>
2023-02-27 19:25:09 +0700

    code

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = '❤️';
+const giftUrl = '';
+const eventName = 'Happy Birthday';
+const titleCard = 'Tặng người ấy';
+const contentCard =
+  'Chúc cậu ngày sinh nhật tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với cậu trong hôm nay và cả những ngày sau';
+
+const giftImage = '/public/imge/Xinhgai.png';
+const base64 = '';
+const giftImageBase64 = 'data:image/png;base64, ' + base64;
