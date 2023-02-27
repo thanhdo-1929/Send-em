9ffbc5b295b37d24901069c7041fd1e59fc2c56a
Thanhdo <dotuanthanhh@gmail.com>
2023-02-27 19:33:45 +0700

    update

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ const titleCard = 'Tặng người ấy';
 const contentCard =
   'Chúc cậu ngày sinh nhật tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với cậu trong hôm nay và cả những ngày sau';
 
-const giftImage = '/public/imge/Xinhgai.png';
+const giftImage = 'Xinhgai1.png';
 const base64 = '';
 const giftImageBase64 = 'data:image/png;base64, ' + base64;
